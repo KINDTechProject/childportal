6f036e34d6f6fd55ce9b4d24372707762b575eb3
Mihir Shah <mihshah@microsoft.com>
2019-07-30 10:13:46 -0700

    making default to spanish

--- src/store/reducers.ts
+++ src/store/reducers.ts
@@ -3,22 +3,25 @@ import { Language } from "./action-types";
 import { LanguageAction } from "./actions";
 
 export interface LanguageState {
-    language: Language;
+  language: Language;
 }
 
 const initialState: LanguageState = {
-    language: Language.en
+  language: Language.es
 };
 
-export type LanguageReducer = (state: LanguageState, action: LanguageAction) => Language;
+export type LanguageReducer = (
+  state: LanguageState,
+  action: LanguageAction
+) => Language;
 
 export const languageReducer: LanguageReducer = (
-    state: LanguageState = initialState,
-    action: LanguageAction
+  state: LanguageState = initialState,
+  action: LanguageAction
 ): Language => {
-    if (typeof get(action, "data") === "string") {
-        return action.data;
-    }
+  if (typeof get(action, "data") === "string") {
+    return action.data;
+  }
 
-    return state.language;
+  return state.language;
 };
